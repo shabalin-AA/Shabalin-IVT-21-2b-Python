685855f3ec42f0c6ddf750dddf21e9304a214949
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2022-12-26 22:39:25 +0500

    Add files via upload

--- lab4/main.py
+++ lab4/main.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+
+def has_cyrillic(text):
+    return bool(re.search('[а-яА-Я]', text))
 
 
 def reduce(list, acc, fn):
@@ -13,19 +19,32 @@ def map(list, fn):
     return list
 
 
+# from handle init data
 l = [1, 2, 3, 4, 5]
-
-#print(reduce(l, 1, lambda x, acc: x * acc))
-#print(map(l, lambda x: x + 5))
+print(map(l, lambda x: x ** 2))
 
 
-titanic = pd.read_csv('titanic.csv', header = 0, delimiter = ',',
-    names = ["PassengerID","Name","PClass","Age","Sex","Survived","SexCode"])[:20]
+# from dataset
+titanic = pd.read_csv('titanic.csv', header = 0, delimiter = ',')[:20]
 
-titanic_male_survived = titanic[(titanic.Sex == 'male') & (titanic.Survived == 1)]
+titanic_male_survived = titanic[(titanic.Sex == 'male') & (titanic.Survived == 1) & (titanic.Age > 0)]
 
-print(titanic_male_survived)
-
-average_age = reduce(titanic_male_survived['Age'], 0, lambda x, acc: x + acc if x > 0 else acc) / len(titanic_male_survived['Age'])
+average_age = reduce(titanic_male_survived['Age'], 0, lambda x, acc: x + acc) / len(titanic_male_survived['Age'])
 
 print(average_age)
+
+
+# from parsed data
+url = 'https://tophit.ru/ru/chart/russia_allmedia/weekly/current/all/all'
+req = requests.get(url)
+soup = bs(req.text, "html.parser")
+chart = soup.find_all("tr", class_ = 'b-chart-item green2')
+
+tracks =[]
+for track in chart:
+    name = track.find('a', class_='black').text
+    radio = track.find('td', class_='bigint').text
+    tracks.append([name, radio.replace(' ', '')])
+
+
+print(reduce(tracks, 0, lambda track, acc: acc + int(track[1]) if has_cyrillic(track[0]) else acc))
